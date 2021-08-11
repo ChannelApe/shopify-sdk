add35b88375e64844a1eb903e270e86b0eecb44d
Khushboo Bhuwalka <khushboobhuwalka2005@gmail.com>
2021-08-11 19:18:26 +0530

    Minor fix

--- src/main/java/com/shopify/model/ShopifyShippingZonesRoot.java
+++ src/main/java/com/shopify/model/ShopifyShippingZonesRoot.java
@@ -1,5 +1,6 @@
 package com.shopify.model;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.List;
 @XmlRootElement
 public class ShopifyShippingZonesRoot {
 
+    @XmlElement(name = "shipping_zones")
     private List<ShopifyShippingZone> shippingZones = new LinkedList<>();
 
     public List<ShopifyShippingZone> getShippingZones() {
